94c85e6db4e9d53b561e0a45c2ec24ef9ba18863
NW100\hosaka <hosaka@suntech.jp>
2026-06-29 15:44:26 +0900

    フルーツクラスとそのテストコードの追加

---
+++ src/Fruit.java
@@ -0,0 +1,23 @@
+public class Fruit {
+    private String name;
+    private int wight;
+
+    public Fruite(string name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public void setName(string name){
+        this.name = name;
+    }
+    public void setWeight(int weight){
+        this.weight = weihgt;
+    }
+
+    public int getName(){
+        return name;
+    }
+    public int getWeight(){
+        return weight;
+    }
+}
---
+++ src/TestFruits.java
@@ -0,0 +1,13 @@
+public class TestFruit {
+    public static void main(String args){
+        Fruit fruits[3];
+
+        fruits[0] = new Fruit("Apple", 10);
+        fruits[1] = new Fruit("Peach", 7);
+        fruits[2] = new Fruit("Grape", 5);
+
+        for(Fruit f : fruits) {
+            System.out.println(f);  //名前と重さを表示する
+        }
+    }
+}
